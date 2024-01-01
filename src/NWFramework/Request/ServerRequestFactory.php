72817330d7d1fcc044a488f46d03a56a8508d870
WalkWeb <av18775@list.ru>
2024-01-01 16:13:15 +0300

    ServerRequestFactory: small improvement

--- src/NWFramework/Request/ServerRequestFactory.php
+++ src/NWFramework/Request/ServerRequestFactory.php
@@ -9,22 +9,22 @@ class ServerRequestFactory
     /**
      * Создает объект Request на основе полученных данных или суперглобальных переменных
      *
-     * TODO Возможно, для минимизации количества подгружаемых файлов и максимализации производительности метод стоит
+     * TODO Для минимизации количества подгружаемых файлов и максимализации производительности метод стоит
      * TODO вынести в сам Request
      *
-     * @param array|null $server
-     * @param array|null $body
-     * @param array|null $cookies
-     * @param array|null $query
-     * @param array|null $files
+     * @param array $server
+     * @param array $body
+     * @param array $cookies
+     * @param array $query
+     * @param array $files
      * @return Request
      */
     public static function fromGlobals(
-        ?array $server = null,
-        ?array $body = null,
-        ?array $cookies = null,
-        ?array $query = null,
-        ?array $files = null
+        array $server = [],
+        array $body = [],
+        array $cookies = [],
+        array $query = [],
+        array $files = []
     ): Request
     {
         return new Request(
