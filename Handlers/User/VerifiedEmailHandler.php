62ab0a3caac979e8864ccbb420487157cb63a702
WalkWeb <av18775@list.ru>
2024-04-07 13:38:01 +0300

    Updated VerifiedEmailHandler

---
+++ Handlers/User/VerifiedEmailHandler.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Handlers\User;
+
+use NW\AbstractHandler;
+use NW\AppException;
+use NW\Request;
+use NW\Response;
+
+class VerifiedEmailHandler extends AbstractHandler
+{
+    /**
+     * Отображает страницу с информацией о необходимости подтвердить email
+     *
+     * Если страницу пытается открыть неавторизованный пользователь - ошибка "вы не авторизованны"
+     *
+     * Если пользователь уже имеет подтвержденный email - сообщение о том, что все ок, и больше ничего делать не нужно
+     *
+     * Если пользователь зарегистрирован, но не подтвердил почту - сообщение о необходимости подтверждения
+     *
+     * @param Request $request
+     * @return Response
+     * @throws AppException
+     */
+    public function __invoke(Request $request): Response
+    {
+        if (!$this->container->exist('user')) {
+            $this->title = 'Необходима авторизация';
+            $message = '<p>Необходима авторизация</p>
+                        <p><a href="/login">Перейти на страницу авторизации</a></p>';
+
+            return $this->render('user/email_verified', ['message' => $message]);
+        }
+
+        if ($this->container->getUser()->isEmailVerified()) {
+            $this->title = 'Email успешно подтвержден';
+            $message = '<p>Вы успешно подтвердили email, ваш аккаунт активирован и все возможности доступны.</p>
+                        <p><a href="/">Перейти на главную</a></p>';
+
+            return $this->render('user/email_verified', ['message' => $message]);
+        }
+
+        $this->title = 'Подтвердите ваш email';
+        $message = '<p>Вам необходимо подтвердить ваш email. Инструкция по активации отправлена на email указанный при регистрации.</p>
+                    <p>Если вам не пришло письмо – свяжитесь с нашей службой поддержки.</p>';
+
+        return $this->render('user/email_verified', ['message' => $message]);
+    }
+}
--- tests/handlers/User/VerifiedEmailHandlerTest.php
+++ tests/handlers/User/VerifiedEmailHandlerTest.php
@@ -10,7 +10,7 @@ use NW\Request;
 use NW\Response;
 use Tests\AbstractTestCase;
 
-class NotVerifiedEmailHandlerTest extends AbstractTestCase
+class VerifiedEmailHandlerTest extends AbstractTestCase
 {
     /**
      * Тест на успешное отображение страницы о необходимости подтвердить email-адрес
@@ -22,12 +22,14 @@ class NotVerifiedEmailHandlerTest extends AbstractTestCase
         $response = $this->app->handle($request);
 
         self::assertEquals(Response::OK, $response->getStatusCode());
+        self::assertRegExp('/Подтвердите ваш email/', $response->getBody());
         self::assertRegExp('/Вам необходимо подтвердить ваш email/', $response->getBody());
 
     }
 
     /**
-     * Тест на ситуацию, когда неавторизованный пользователь пытается открыть страницу - его переадресовывает на главную
+     * Тест на ситуацию, когда неавторизованный пользователь пытается открыть страницу - ему сообщается, нужно
+     * авторизоваться
      *
      * @throws AppException
      */
@@ -36,12 +38,13 @@ class NotVerifiedEmailHandlerTest extends AbstractTestCase
         $request = new Request(['REQUEST_URI' => '/verified_email']);
         $response = $this->app->handle($request);
 
-        self::assertEquals(Response::FOUND, $response->getStatusCode());
+        self::assertEquals(Response::OK, $response->getStatusCode());
+        self::assertRegExp('/Необходима авторизация/', $response->getBody());
+        self::assertRegExp('/Перейти на страницу авторизации/', $response->getBody());
     }
 
     /**
-     * Тест на ситуацию, когда пользователь с подтвержденной почтой пытается открыть страницу - его переадресовывает на
-     * главную
+     * Тест на ситуацию, когда пользователь с подтвержденной почтой пытается открыть страницу - ему сообщается что все ок
      *
      * @throws AppException
      */
@@ -51,6 +54,8 @@ class NotVerifiedEmailHandlerTest extends AbstractTestCase
         $request = new Request(['REQUEST_URI' => '/verified_email'], [], [UserInterface::AUTH_TOKEN => $authToken]);
         $response = $this->app->handle($request);
 
-        self::assertEquals(Response::FOUND, $response->getStatusCode());
+        self::assertEquals(Response::OK, $response->getStatusCode());
+        self::assertRegExp('/Email успешно подтвержден/', $response->getBody());
+        self::assertRegExp('/Вы успешно подтвердили email, ваш аккаунт активирован и все возможности доступны/', $response->getBody());
     }
 }
