262263962a831e4371b0b656acf7a3d57f4708cf
WalkWeb <av18775@list.ru>
2024-04-20 21:41:01 +0300

    Improved CookieDeleteHandler

--- Handlers/Cookie/CookieDeleteHandler.php
+++ Handlers/Cookie/CookieDeleteHandler.php
@@ -8,9 +8,12 @@ use NW\AbstractHandler;
 use NW\AppException;
 use NW\Request;
 use NW\Response;
+use NW\Traits\ValidationTrait;
 
 class CookieDeleteHandler extends AbstractHandler
 {
+    use ValidationTrait;
+
     /**
      * Удаляет указанные куки
      *
@@ -20,12 +23,36 @@ class CookieDeleteHandler extends AbstractHandler
      */
     public function __invoke(Request $request): Response
     {
-        $data = $request->getBody();
+        try {
+            $data = $this->validateData($request->getBody());
 
-        // TODO Валидация body
+            $this->container->getCookies()->delete((string)$data['name']);
 
-        $this->container->getCookies()->delete((string)$data['name']);
+            return $this->redirect('/cookies');
+        } catch (AppException $e) {
+            return $this->render('/cookies/index', [
+                'cookies' => $this->container->getRequest()->getCookies()->getArray(),
+                'error'   => $e->getMessage(),
+            ]);
+        }
+    }
 
-        return $this->redirect('/cookies');
+    /**
+     * @param array $data
+     * @return array
+     * @throws AppException
+     */
+    private function validateData(array $data): array
+    {
+        self::string($data, 'name', 'Bad request: "name" value required and expected string');
+
+        self::stringMinMaxLength(
+            $data['name'],
+            1,
+            50,
+            'Length "name" parameter must be from 1 to 50 characters',
+        );
+
+        return $data;
     }
 }
--- tests/handlers/Cookie/CookieDeleteHandlerTest.php
+++ tests/handlers/Cookie/CookieDeleteHandlerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\handlers\Cookie;
 
+use Exception;
 use NW\AppException;
 use NW\Request;
 use NW\Response;
@@ -29,5 +30,67 @@ class CookieDeleteHandlerTest extends AbstractTest
         self::assertEquals(Response::FOUND, $response->getStatusCode());
         // Куки стали пустыми
         self::assertEquals([], $this->app->getContainer()->getCookies()->getArray());
+    }
+
+
+    /**
+     * Тесты на различные варианты невалидных данных
+     *
+     * @dataProvider failDataProvider
+     * @param array $data
+     * @param string $error
+     * @throws AppException
+     */
+    public function testCookieDeleteHandlerFail(array $data, string $error): void
+    {
+        $cookie = ['xxx' => 'xxx'];
+        $request = new Request(
+            ['REQUEST_URI' => '/cookies/delete', 'REQUEST_METHOD' => 'POST'],
+            $data,
+            $cookie,
+        );
+        $response = $this->app->handle($request);
+
+        self::assertEquals(Response::OK, $response->getStatusCode());
+        self::assertRegExp("/$error/", $response->getBody());
+
+        // Проверяем, что куки не исчезли
+        self::assertEquals($cookie, $this->app->getContainer()->getCookies()->getArray());
+    }
+
+    /**
+     * @return array[]
+     * @throws Exception
+     */
+    public function failDataProvider(): array
+    {
+        return [
+            // Отсутствует name
+            [
+                [],
+                'Error: Bad request: "name" value required and expected string',
+            ],
+            // name некорректного типа
+            [
+                [
+                    'name'  => 123,
+                ],
+                'Error: Bad request: "name" value required and expected string',
+            ],
+            // name пустая строка
+            [
+                [
+                    'name'  => '',
+                ],
+                'Length "name" parameter must be from 1 to 50 characters',
+            ],
+            // name больше максимальной длинны
+            [
+                [
+                    'name'  => self::generateString(51),
+                ],
+                'Length "name" parameter must be from 1 to 50 characters',
+            ],
+        ];
     }
 }
