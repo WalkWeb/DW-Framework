ef72fec4b553bd855643607c4228ef1b58f59e7b
WalkWeb <av18775@list.ru>
2025-01-16 13:25:13 +0300

    PaginationTrait: added onclick function

--- src/Traits/PaginationTrait.php
+++ src/Traits/PaginationTrait.php
@@ -11,9 +11,16 @@ trait PaginationTrait
      * @param int $page
      * @param string $url
      * @param int $perPage
+     * @param string $onClick
      * @return string
      */
-    public function getPages(int $elements, int $page, string $url, int $perPage = 5): string
+    public function getPages(
+        int $elements,
+        int $page,
+        string $url,
+        int $perPage = 5,
+        string $onClick = ''
+    ): string
     {
         $numberPage = (int)ceil($elements / $perPage);
         $content = '';
@@ -30,11 +37,14 @@ trait PaginationTrait
                 $content .= ' <span>' . $i . '</span> ';
             } else {
                 if ($i < ($page - 3)) {
-                    $left = ' <a href="' . $url . '" title="">1</a> ' . (($page - 4 <= $numberPage) ? '...' : '');
+                    $link = $onClick ? ' <span onclick="' . $onClick . '(1)" class="link">1</span> ' : ' <a href="' . $url . '" title="">1</a> ';
+                    $left = $link . (($page - 4 <= $numberPage) ? '...' : '');
                 } elseif ($i > ($page + 3)) {
-                    $right = (($page + 4 >= $numberPage) ? '' : '...') . ' <a href="' . $url . $numberPage . '" title="">' . $numberPage . '</a> ';
+                    $link = $onClick ? ' <span onclick="' . $onClick . '(' . $numberPage . ')" class="link">' . $numberPage . '</span> ' : ' <a href="' . $url . $numberPage . '" title="">' . $numberPage . '</a> ';
+                    $right = (($page + 4 >= $numberPage) ? '' : '...') . $link;
                 } else {
-                    $content .= ' <a href="' . $url . $i . '" title="">' . $i . '</a> ';
+                    $link = $onClick ? ' <span onclick="' . $onClick . '(' . $i . ')" class="link">' . $i . '</span> ' : ' <a href="' . $url . $i . '" title="">' . $i . '</a> ';
+                    $content .= $link;
                 }
             }
             $i++;
--- tests/src/Traits/PaginationTraitTest.php
+++ tests/src/Traits/PaginationTraitTest.php
@@ -16,7 +16,7 @@ class PaginationTraitTest extends AbstractTest
         self::assertEquals('', $this->getPages(10, 1, 'pages', 20));
     }
 
-    public function testPaginationFirstPage(): void
+    public function testPaginationFirstPageDefault(): void
     {
         $elements = 100;
         $page = 1;
@@ -29,7 +29,21 @@ class PaginationTraitTest extends AbstractTest
         );
     }
 
-    public function testPaginationLastPage(): void
+    public function testPaginationFirstPageOnClick(): void
+    {
+        $elements = 100;
+        $page = 1;
+        $url = '';
+        $perPage = 10;
+        $onclick = 'getComments';
+
+        self::assertEquals(
+            '<span>1</span>  <span onclick="getComments(2)" class="link">2</span>  <span onclick="getComments(3)" class="link">3</span>  <span onclick="getComments(4)" class="link">4</span> ... <span onclick="getComments(10)" class="link">10</span>',
+            $this->getPages($elements, $page, $url, $perPage, $onclick)
+        );
+    }
+
+    public function testPaginationLastPageDefault(): void
     {
         $elements = 100;
         $page = 10;
@@ -42,7 +56,21 @@ class PaginationTraitTest extends AbstractTest
         );
     }
 
-    public function testPaginationMiddlePage(): void
+    public function testPaginationLastPageOnClick(): void
+    {
+        $elements = 100;
+        $page = 10;
+        $url = '';
+        $perPage = 10;
+        $onclick = 'getComments';
+
+        self::assertEquals(
+            '<span onclick="getComments(1)" class="link">1</span> ... <span onclick="getComments(7)" class="link">7</span>  <span onclick="getComments(8)" class="link">8</span>  <span onclick="getComments(9)" class="link">9</span>  <span>10</span>',
+            $this->getPages($elements, $page, $url, $perPage, $onclick)
+        );
+    }
+
+    public function testPaginationMiddlePageDefault(): void
     {
         $elements = 200;
         $page = 10;
@@ -52,6 +80,20 @@ class PaginationTraitTest extends AbstractTest
         self::assertEquals(
             '<a href="pages/" title="">1</a> ... <a href="pages/7" title="">7</a>  <a href="pages/8" title="">8</a>  <a href="pages/9" title="">9</a>  <span>10</span>  <a href="pages/11" title="">11</a>  <a href="pages/12" title="">12</a>  <a href="pages/13" title="">13</a> ... <a href="pages/20" title="">20</a>',
             $this->getPages($elements, $page, $url, $perPage)
+        );
+    }
+
+    public function testPaginationMiddlePageOnClick(): void
+    {
+        $elements = 200;
+        $page = 10;
+        $url = '';
+        $perPage = 10;
+        $onclick = 'getComments';
+
+        self::assertEquals(
+            '<span onclick="getComments(1)" class="link">1</span> ... <span onclick="getComments(7)" class="link">7</span>  <span onclick="getComments(8)" class="link">8</span>  <span onclick="getComments(9)" class="link">9</span>  <span>10</span>  <span onclick="getComments(11)" class="link">11</span>  <span onclick="getComments(12)" class="link">12</span>  <span onclick="getComments(13)" class="link">13</span> ... <span onclick="getComments(20)" class="link">20</span>',
+            $this->getPages($elements, $page, $url, $perPage, $onclick)
         );
     }
 }
